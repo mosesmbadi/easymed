db2e90c5335b9540b906fc7966d79a14d7a93749
clairemuiru1 <muiruclaire@gmail.com>
2025-03-17 20:44:48 +0300

    Revert ""reset password complete""
    This reverts commit 4148d75ccc685d74537e8392e45bd6ae6695c1f2.

--- front-end/src/pages/auth/login.js
+++ front-end/src/pages/auth/login.js
@@ -126,10 +126,6 @@ const Login = () => {
                 )}
                 Login
               </button>
-              <div className="flex items-center gap-4">
-              <p>Forgot Password ? </p>
-              <span className="text-primary_light"><Link href="/auth/forgot-password"> Reset </Link></span>
-              </div>
               <div className="flex gap-4">
                 <p>Do you have an account ? </p>
                 <span className="text-primary_light"><Link href="/auth/register"> SignUp </Link></span>
