b54315a37ec57508ae85072940f306f77ffb1334
mosesmbadi <mosesmbadi@gmail.com>
2025-09-30 01:34:17 +0300

    Fix token issue,

--- front-end/src/assets/hoc/protected-route.js
+++ front-end/src/assets/hoc/protected-route.js
@@ -1,24 +1,33 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import NotAuthorized from './not-authorised'
+import { useAuth } from '../hooks/use-auth'
+import { useContext } from 'react'
+import { authContext } from '@/components/use-context'
 
+const ProtectedRoute = ({ permission, children }) => {
+    const { userPermissions } = useSelector((store) => store.auth);
+    const auth = useAuth();
+    const { isTokenValid } = useContext(authContext);
 
-const ProtectedRoute = ({ permission,children }) => {
-    const { userPermissions } = useSelector(( store ) => store.auth);
-
-
-    // check if current user has permission to access the route in question
-    const isAuthorized = userPermissions && userPermissions.find((perm) => perm === permission)
-
-    if (!isAuthorized) {
-      return <NotAuthorized />
+    // Check if user is authenticated and has valid token
+    if (!auth?.token || !auth?.user_id || !isTokenValid(auth.token)) {
+        return <NotAuthorized />
     }
 
-  return (
-    <>{ children }</>
-  )
+    // Ensure userPermissions is an array before using array methods
+    const permissions = Array.isArray(userPermissions) ? userPermissions : [];
+
+    // Check if current user has permission to access the route in question
+    const isAuthorized = permissions.find((perm) => perm === permission)
+
+    if (!isAuthorized) {
+        return <NotAuthorized />
+    }
+
+    return (
+        <>{children}</>
+    )
 }
-
-
 
 export default ProtectedRoute
