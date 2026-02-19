eeb8a421130baf0815668b258a20691451e57093
levy <l.njiru43@gmail.com>
2026-02-19 12:57:23 +0300

    add manual sending to lab as default and only send to equipment when equipment is selected

--- front-end/src/components/dashboard/laboratory/TestPanelsItem.js
+++ front-end/src/components/dashboard/laboratory/TestPanelsItem.js
@@ -5,29 +5,16 @@ import {
   fetchLabTestPanelsBySpecificSample,
   updatePhlebotomySamples,
 } from "@/redux/service/laboratory";
-import SeachableSelect from "@/components/select/Searchable";
-import { Form, Formik } from "formik";
-import * as Yup from "yup";
-import FormButton from "@/components/common/button/FormButton";
-import { sendToEquipment } from "@/redux/service/laboratory";
-import { toast } from "react-toastify";
+import ResultPanelRow from "./ResultPanelRow";
 
 const TestPanelsItem = ({ sample, collected, sample_id }) => {
   const auth = useAuth();
-  const [loading, setLoading] = useState(false);
+
   const [resultItems, setResultItems] = useState([]);
-  const [selectedEquip, setSelectedEquip] = useState(null);
-  const { labTestPanels, labEquipments } = useSelector(
+  const { labTestPanels } = useSelector(
     (store) => store.laboratory,
   );
 
-  const initialValues = {
-    equipment: "",
-  };
-
-  const validationSchema = Yup.object().shape({
-    equipment: Yup.object().required("This field is required!"),
-  });
 
   const getTestPanelsBySampleId = async (sample, auth) => {
     try {
@@ -35,44 +22,6 @@ const TestPanelsItem = ({ sample, collected, sample_id }) => {
       setResultItems(response);
     } catch (error) {
       console.log("ERROR GETTING PANELS");
-    }
-  };
-
-  const approveCollection = async () => {
-    try {
-      const payload = {
-        is_sample_collected: true,
-        id: sample_id,
-      };
-      await updatePhlebotomySamples(payload, auth);
-    } catch (error) {
-      console.log("ERR APPROVING COLLECTION OF SAMPLE", error);
-    }
-  };
-
-  const handleSendEquipment = async (foundPanel, formValue, helpers) => {
-    if (formValue.equipment.value === "manual") {
-      approveCollection();
-      return;
-    }
-    try {
-      const formData = {
-        ...foundPanel,
-        patient_id: foundPanel.patient_id,
-        equipment: formValue.equipment.label,
-      };
-      console.log(formData);
-      setLoading(true);
-      approveCollection();
-      await sendToEquipment(formData).then(() => {
-        helpers?.resetForm();
-        toast.success("Send to Equipment Successful!");
-        setLoading(false);
-      });
-    } catch (err) {
-      toast.error("Failed to send to equipment");
-      console.log("ERROR SENDING TO EQUIPMENT", err);
-      setLoading(false);
     }
   };
 
@@ -88,56 +37,7 @@ const TestPanelsItem = ({ sample, collected, sample_id }) => {
     );
     if (foundPanel) {
       return (
-        <li key={`${foundPanel.id}_panel`}>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={(values, { resetForm }) => {
-              handleSendEquipment(item, values, { resetForm });
-            }}
-          >
-            {({ handleSubmit, values }) => (
-              <form
-                onSubmit={handleSubmit}
-                className="w-full flex items-center bg-red-400"
-              >
-                <span className="w-10/12">{foundPanel.name}</span>
-                <div className="w-full">
-                  <SeachableSelect
-                    name="equipment"
-                    setSelectedItem={(value) => setSelectedEquip(value)}
-                    options={[
-                      { value: "manual", label: "Manual" },
-                      ...labEquipments.map((equipment) => ({
-                        value: equipment.id,
-                        label: equipment.name,
-                      })),
-                    ]}
-                  />
-                </div>
-                {item.is_billed ? (
-                    <div className="w-full justify-end flex">
-                      <button
-                        disabled={!values.equipment || loading}
-                        type="submit"
-                        className={`${!values.equipment ? "bg-gray cursor-not-allowed" : "bg-primary"} text-white cursor-pointer w-10/12 text-center py-2 text-xs rounded-xl flex justify-center items-center`}
-                      >
-                        {loading ? "Sending..." : "send"}
-                      </button>
-                  </div>
-                ) : (
-                  <>
-                    <div className="w-full justify-center flex">
-                      <div className="w-10/12 bg-primary text-white cursor-default px-3 py-2 text-xs rounded-xl text-center">
-                        Not Applicable
-                      </div>
-                    </div>
-                  </>
-                )}
-              </form>
-            )}
-          </Formik>
-        </li>
+        <ResultPanelRow key={`${foundPanel.id}_panel`} item={item} foundPanel={foundPanel} sample_id={sample_id} collected={collected}/>
       );
     }
   });
